a35b1c09430efe682a3dc470a995d4424a61b453
Zhu Jiongyao <zjywade@qq.com>
2017-02-22 23:55:48 +0800

    add _basedir to config

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 # coding: utf-8
+import os
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = False
+
 SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost:3306/flask_skeleton_db?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 try:
-    from local_settings import * # 导入本地配置
+    from local_settings import *  # 导入本地配置
 except ImportError:
     pass
+
+del os
